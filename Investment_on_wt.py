abc5af6b06f3e0b9f4697d20c99040edd0be13eb
Maddiboina Pavani Divya <124232885+Maddiboinapav-ani@users.noreply.github.com>
2024-10-16 19:45:23 +0530

    Add files via upload

---
+++ Investment_on_wt.py
@@ -0,0 +1,61 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+dataset=pd.read_csv(r"C:\Users\user\Documents\Investment.csv")
+X = dataset.iloc[:, :-1]
+
+y = dataset.iloc[:, 4]
+
+X = pd.get_dummies(X,dtype=int)
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train,y_train)
+
+y_pred = regressor.predict(X_test)
+
+#== we build mlr model
+
+m = regressor.coef_
+print(m)
+
+c = regressor.intercept_
+print(c)
+
+X=np.append(arr=np.ones((50,1)).astype(int),values=X,axis=1)
+
+import statsmodels.api as sm
+X_opt=X[:,[0,1,2,3,4,5]]
+regressor_OLS=sm.OLS(endog=y,exog=X_opt).fit()
+regressor_OLS.summary()
+
+import statsmodels.api as sm
+X_opt=X[:,[0,1,2,3,5]]
+regressor_OLS=sm.OLS(endog=y,exog=X_opt).fit()
+regressor_OLS.summary()
+
+import statsmodels.api as sm
+X_opt=X[:,[0,1,2,3]]
+regressor_OLS=sm.OLS(endog=y,exog=X_opt).fit()
+regressor_OLS.summary()
+
+
+import statsmodels.api as sm
+X_opt=X[:,[0,1,3]]
+regressor_OLS=sm.OLS(endog=y,exog=X_opt).fit()
+regressor_OLS.summary()
+
+
+import statsmodels.api as sm
+X_opt=X[:,[0,1]]
+regressor_OLS=sm.OLS(endog=y,exog=X_opt).fit()
+regressor_OLS.summary()
+
+
+
+
+
